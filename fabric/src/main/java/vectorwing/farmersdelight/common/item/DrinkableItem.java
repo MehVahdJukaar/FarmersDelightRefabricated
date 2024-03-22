e4f6727ac109c9c819445319f758fb495b8ea61a
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2024-03-23 01:01:06 +1100

    Fabric does not have stack specific item edibility yet.

--- fabric/src/main/java/vectorwing/farmersdelight/common/item/DrinkableItem.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/item/DrinkableItem.java
@@ -2,7 +2,6 @@ package vectorwing.farmersdelight.common.item;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
@@ -24,20 +23,21 @@ public class DrinkableItem extends ConsumableItem
 	}
 
 	@Override
+	public int getUseDuration(ItemStack stack) {
+		return 32;
+	}
+
+	@Override
 	public UseAnim getUseAnimation(ItemStack stack) {
 		return UseAnim.DRINK;
 	}
 
 	@Override
-	public int getUseDuration(ItemStack stack, LivingEntity entity) {
-		return 32;
-	}
-
-	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack heldStack = player.getItemInHand(hand);
-		if (heldStack.getFoodProperties(player) != null) {
-			if (player.canEat(heldStack.getFoodProperties(player).canAlwaysEat())) {
+		if (heldStack.isEdible()) {
+			// TODO: >1.20.1, use https://github.com/FabricMC/fabric/pull/3520 instead of this.
+			if (player.canEat(heldStack.getItem().getFoodProperties().canAlwaysEat())) {
 				player.startUsingItem(hand);
 				return InteractionResultHolder.consume(heldStack);
 			} else {
