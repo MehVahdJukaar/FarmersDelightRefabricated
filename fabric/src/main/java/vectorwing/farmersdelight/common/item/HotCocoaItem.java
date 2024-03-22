0aad0e8ffa80a036ed77327887d478d5f204a6b4
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2024-03-23 01:04:07 +1100

    Remove references to MobEffectEvent, as the equivalent is mixins.

--- fabric/src/main/java/vectorwing/farmersdelight/common/item/HotCocoaItem.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/item/HotCocoaItem.java
@@ -1,14 +1,13 @@
 package vectorwing.farmersdelight.common.item;
 
-import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.EffectCures;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -22,18 +21,19 @@ public class HotCocoaItem extends DrinkableItem
 	@Override
 	public void affectConsumer(ItemStack stack, Level level, LivingEntity consumer) {
 		Iterator<MobEffectInstance> itr = consumer.getActiveEffects().iterator();
-		ArrayList<Holder<MobEffect>> compatibleEffects = new ArrayList<>();
+		ArrayList<MobEffect> compatibleEffects = new ArrayList<>();
 
 		while (itr.hasNext()) {
 			MobEffectInstance effect = itr.next();
-			if (effect.getEffect().value().getCategory().equals(MobEffectCategory.HARMFUL) && effect.getCures().contains(EffectCures.MILK)) {
+			if (effect.getEffect().getCategory().equals(MobEffectCategory.HARMFUL) && effect.isCurativeItem(new ItemStack(Items.MILK_BUCKET))) {
 				compatibleEffects.add(effect.getEffect());
 			}
 		}
 
 		if (compatibleEffects.size() > 0) {
 			MobEffectInstance selectedEffect = consumer.getEffect(compatibleEffects.get(level.random.nextInt(compatibleEffects.size())));
-			if (selectedEffect != null && !net.neoforged.neoforge.event.EventHooks.onEffectRemoved(consumer, selectedEffect, EffectCures.MILK)) {
+			// There is no equivalent for MobEffectEvent, people are expected to mixin with instances like this on Fabric, so we don't bother.
+			if (selectedEffect != null) {
 				consumer.removeEffect(selectedEffect.getEffect());
 			}
 		}
--- fabric/src/main/java/vectorwing/farmersdelight/common/item/MilkBottleItem.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/item/MilkBottleItem.java
@@ -1,12 +1,11 @@
 package vectorwing.farmersdelight.common.item;
 
-import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.EffectCures;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,18 +19,19 @@ public class MilkBottleItem extends DrinkableItem
 	@Override
 	public void affectConsumer(ItemStack stack, Level level, LivingEntity consumer) {
 		Iterator<MobEffectInstance> itr = consumer.getActiveEffects().iterator();
-		ArrayList<Holder<MobEffect>> compatibleEffects = new ArrayList<>();
+		ArrayList<MobEffect> compatibleEffects = new ArrayList<>();
 
 		while (itr.hasNext()) {
 			MobEffectInstance effect = itr.next();
-			if (effect.getCures().contains(EffectCures.MILK)) {
+			if (effect.isCurativeItem(new ItemStack(Items.MILK_BUCKET))) {
 				compatibleEffects.add(effect.getEffect());
 			}
 		}
 
-		if (!compatibleEffects.isEmpty()) {
+		if (compatibleEffects.size() > 0) {
 			MobEffectInstance selectedEffect = consumer.getEffect(compatibleEffects.get(level.random.nextInt(compatibleEffects.size())));
-			if (selectedEffect != null && !net.neoforged.neoforge.event.EventHooks.onEffectRemoved(consumer, selectedEffect, EffectCures.MILK)) {
+			// There is no equivalent for MobEffectEvent, people are expected to mixin with instances like this on Fabric, so we don't bother.
+			if (selectedEffect != null) {
 				consumer.removeEffect(selectedEffect.getEffect());
 			}
 		}
