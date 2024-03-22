13de45f32a5f6d021b9a4bdee6151bf3af9dd5e0
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2024-03-23 02:44:41 +1100

    Remove erroring imports.

--- fabric/src/main/java/vectorwing/farmersdelight/common/world/modifier/AddFeaturesByFilterBiomeModifier.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/world/modifier/AddFeaturesByFilterBiomeModifier.java
@@ -1,15 +1,9 @@
 package vectorwing.farmersdelight.common.world.modifier;
 
-import com.mojang.serialization.MapCodec;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.neoforged.neoforge.common.world.BiomeGenerationSettingsBuilder;
-import net.neoforged.neoforge.common.world.BiomeModifier;
-import net.neoforged.neoforge.common.world.ModifiableBiomeInfo;
-import vectorwing.farmersdelight.common.registry.ModBiomeModifiers;
 
 import java.util.Optional;
 
@@ -30,10 +24,10 @@ public record AddFeaturesByFilterBiomeModifier(
 			if (deniedBiomes.isPresent() && this.deniedBiomes.get().contains(biome)) {
 				return;
 			}
-			if (minimumTemperature.isPresent() && biome.value().getBaseTemperature() < minimumTemperature.get()) {
+			if (minimumTemperature.isPresent() && biome.get().getBaseTemperature() < minimumTemperature.get()) {
 				return;
 			}
-			if (maximumTemperature.isPresent() && biome.value().getBaseTemperature() > maximumTemperature.get()) {
+			if (maximumTemperature.isPresent() && biome.get().getBaseTemperature() > maximumTemperature.get()) {
 				return;
 			}
 			BiomeGenerationSettingsBuilder generationSettings = builder.getGenerationSettings();
@@ -42,7 +36,7 @@ public record AddFeaturesByFilterBiomeModifier(
 	}
 
 	@Override
-	public MapCodec<? extends BiomeModifier> codec() {
+	public Codec<? extends BiomeModifier> codec() {
 		return ModBiomeModifiers.ADD_FEATURES_BY_FILTER.get();
 	}
 	 */
