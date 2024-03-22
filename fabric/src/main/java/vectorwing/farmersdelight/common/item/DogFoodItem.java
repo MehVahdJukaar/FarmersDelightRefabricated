e61f5549ea2ff577505924c9ed7c9d2341cc005e
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-03-22 14:56:06 +0100

    dog food event

--- fabric/src/main/java/vectorwing/farmersdelight/common/item/DogFoodItem.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/item/DogFoodItem.java
@@ -1,6 +1,7 @@
 package vectorwing.farmersdelight.common.item;
 
 import com.google.common.collect.Lists;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -20,10 +21,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import vectorwing.farmersdelight.FarmersDelight;
+import net.minecraft.world.phys.EntityHitResult;
+import org.jetbrains.annotations.Nullable;
 import vectorwing.farmersdelight.common.Configuration;
 import vectorwing.farmersdelight.common.registry.ModItems;
 import vectorwing.farmersdelight.common.registry.ModParticleTypes;
@@ -31,8 +30,9 @@ import vectorwing.farmersdelight.common.tag.ModTags;
 import vectorwing.farmersdelight.common.utility.MathUtils;
 import vectorwing.farmersdelight.common.utility.TextUtils;
 
-import org.jetbrains.annotations.Nullable;;
 import java.util.List;
+
+;
 
 public class DogFoodItem extends ConsumableItem
 {
@@ -45,15 +45,18 @@ public class DogFoodItem extends ConsumableItem
 		super(properties);
 	}
 
-	@EventBusSubscriber(modid = FarmersDelight.MODID, bus = EventBusSubscriber.Bus.GAME)
+	public static void init(){
+		UseEntityCallback.EVENT.register(DogFoodItem.DogFoodEvent::onDogFoodApplied);
+	}
+
 	public static class DogFoodEvent
 	{
-		@SubscribeEvent
-		@SuppressWarnings("unused")
-		public static void onDogFoodApplied(PlayerInteractEvent.EntityInteract event) {
-			Player player = event.getEntity();
-			Entity target = event.getTarget();
-			ItemStack itemStack = event.getItemStack();
+
+		public static InteractionResult onDogFoodApplied(Player player, Level level, InteractionHand hand, Entity target,
+														 @Nullable EntityHitResult entityHitResult) {
+			if (player.isSpectator()) return InteractionResult.PASS;
+
+			ItemStack itemStack = player.getItemInHand(hand);
 
 			if (target instanceof LivingEntity entity && target.getType().is(ModTags.DOG_FOOD_USERS)) {
 				boolean isTameable = entity instanceof TamableAnimal;
@@ -77,15 +80,16 @@ public class DogFoodItem extends ConsumableItem
 						itemStack.shrink(1);
 					}
 
-					event.setCancellationResult(InteractionResult.SUCCESS);
-					event.setCanceled(true);
+					return InteractionResult.sidedSuccess(level.isClientSide);
 				}
 			}
+			return InteractionResult.PASS;
 		}
+
 	}
 
 	@Override
-	public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag isAdvanced) {
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag isAdvanced) {
 		if (!Configuration.FOOD_EFFECT_TOOLTIP.get()) {
 			return;
 		}
@@ -97,14 +101,14 @@ public class DogFoodItem extends ConsumableItem
 			MutableComponent effectDescription = Component.literal(" ");
 			MutableComponent effectName = Component.translatable(effectInstance.getDescriptionId());
 			effectDescription.append(effectName);
-			MobEffect effect = effectInstance.getEffect().value();
+			MobEffect effect = effectInstance.getEffect();
 
 			if (effectInstance.getAmplifier() > 0) {
 				effectDescription.append(" ").append(Component.translatable("potion.potency." + effectInstance.getAmplifier()));
 			}
 
 			if (effectInstance.getDuration() > 20) {
-				effectDescription.append(" (").append(MobEffectUtil.formatDuration(effectInstance, 1.0F, context.tickRate())).append(")");
+				effectDescription.append(" (").append(MobEffectUtil.formatDuration(effectInstance, 1.0F)).append(")");
 			}
 
 			tooltip.add(effectDescription.withStyle(effect.getCategory().getTooltipFormatting()));
