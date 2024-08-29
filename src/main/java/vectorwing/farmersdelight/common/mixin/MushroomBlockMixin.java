b3f0fbb482a5a30359eb85b397db55e82f3629b8
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-08-30 09:58:10 +1000

    Updates to Rich Soil logic.

--- src/main/java/vectorwing/farmersdelight/common/mixin/MushroomBlockMixin.java
+++ src/main/java/vectorwing/farmersdelight/common/mixin/MushroomBlockMixin.java
@@ -1,10 +1,10 @@
 package vectorwing.farmersdelight.common.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.MushroomBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,10 +16,10 @@ import vectorwing.farmersdelight.common.tag.ModTags;
  * Fabric should <b>really</b> have an event for this...
  * This is the bare minimum to keep parity with Forge.
  */
-@Mixin(BushBlock.class)
-public class BushBlockMixin {
-    @ModifyReturnValue(method = "canSurvive", at = @At("RETURN"))
-    private boolean farmersdelightrefabricated$allowPlantsOnBushes(boolean original, BlockState state, LevelReader level, BlockPos pos) {
+@Mixin(MushroomBlock.class)
+public class MushroomBlockMixin {
+    @ModifyExpressionValue(method = "canSurvive", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/MushroomBlock;mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z"))
+    private boolean farmersdelightrefabricated$allowPlantsOnMushroom(boolean original, BlockState state, LevelReader level, BlockPos pos) {
         if (state.getBlock() != (Object)this)
             return original;
 
