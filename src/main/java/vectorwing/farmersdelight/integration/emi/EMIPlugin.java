c21a70e8bbf3f71a4edad3cb723731821de4ac9d
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2025-03-19 17:07:17 +1100

    Add default EMI recipes, remove TODO comment in EMIPlugin.

--- src/main/java/vectorwing/farmersdelight/integration/emi/EMIPlugin.java
+++ src/main/java/vectorwing/farmersdelight/integration/emi/EMIPlugin.java
@@ -28,8 +28,6 @@ public class EMIPlugin implements EmiPlugin {
         registry.addWorkstation(FDRecipeCategories.COOKING, FDRecipeWorkstations.COOKING_POT);
         registry.addWorkstation(FDRecipeCategories.CUTTING, FDRecipeWorkstations.CUTTING_BOARD);
         registry.addRecipeHandler(ModMenuTypes.COOKING_POT.get(), new CookingPotEmiRecipeHandler());
-        //TODO: add ability to client on recipe arrow. we can also do it directly from recipe screen
-
 
         for (RecipeHolder<CookingPotRecipe> recipe : registry.getRecipeManager().getAllRecipesFor(ModRecipeTypes.COOKING.get())) {
             registry.addRecipe(new CookingPotEmiRecipe(recipe.id(), recipe.value().getIngredients().stream().map(EmiIngredient::of).toList(),
