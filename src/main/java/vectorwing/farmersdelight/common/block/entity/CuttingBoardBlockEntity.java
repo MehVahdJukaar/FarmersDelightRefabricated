78d50924efd3b9d425eb3d627737ef58a8d01b49
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2025-03-20 21:39:09 +1100

    Fix #154 and other cutting board bugs.

--- src/main/java/vectorwing/farmersdelight/common/block/entity/CuttingBoardBlockEntity.java
+++ src/main/java/vectorwing/farmersdelight/common/block/entity/CuttingBoardBlockEntity.java
@@ -170,7 +170,7 @@ public class CuttingBoardBlockEntity extends SyncedBlockEntity
 			isItemCarvingBoard = false;
 			ItemStack item = getStoredItem().split(1);
 			inventory.commitModifiedStacks();
-			inventoryChanged();
+//			inventoryChanged();
 			return item;
 		}
 		return ItemStack.EMPTY;
