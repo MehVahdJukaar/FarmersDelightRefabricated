c45b0e3136b47fa7d2ac66c88a2a54eda9081ddc
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2025-06-08 03:29:41 +1000

    Fix #77.

--- src/main/java/vectorwing/farmersdelight/FarmersDelightASM.java
+++ src/main/java/vectorwing/farmersdelight/FarmersDelightASM.java
@@ -1,13 +1,15 @@
 package vectorwing.farmersdelight;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
-import java.util.List;
+import java.util.function.Supplier;
 
 public class FarmersDelightASM implements Runnable {
     public static final String COOKING_RECIPE_BOOK_TYPE = "FARMERSDELIGHT_COOKING";
@@ -21,12 +23,31 @@ public class FarmersDelightASM implements Runnable {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
         String recipeBookTypeTarget = remapper.mapClassName("intermediary", "net.minecraft.class_5421");
         ClassTinkerers.enumBuilder(recipeBookTypeTarget).addEnum(COOKING_RECIPE_BOOK_TYPE).build();
-        String recipeBookCategoriesTarget = remapper.mapClassName("intermediary", "net.minecraft.class_314");
-        String itemStackParamType = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_SEARCH_RECIPE_BOOK_CATEGORY, () -> new Object[]{new ItemStack[]{new ItemStack(Items.COMPASS)}}).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MEALS_RECIPE_BOOK_CATEGORY, () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.VEGETABLE_NOODLES.get())}}).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_DRINKS_RECIPE_BOOK_CATEGORY,() -> new Object[]{new ItemStack[]{new ItemStack(ModItems.APPLE_CIDER.get())}}).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MISC_RECIPE_BOOK_CATEGORY, () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.DUMPLINGS.get()), new ItemStack(ModItems.TOMATO_SAUCE.get())}}).build();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            String recipeBookCategoriesTarget = remapper.mapClassName("intermediary", "net.minecraft.class_314");
+            String itemStackParamType = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_SEARCH_RECIPE_BOOK_CATEGORY, getSearchCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MEALS_RECIPE_BOOK_CATEGORY, getMealsCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_DRINKS_RECIPE_BOOK_CATEGORY, getDrinksCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MISC_RECIPE_BOOK_CATEGORY, getMiscCategoryStacks()).build();
+        }
     }
 
+    public static Supplier<Object[]> getSearchCategoryStacks() {
+        // Requires a Supplier based workaround to make sure that ItemLike isn't loaded.
+        // See https://github.com/MehVahdJukaar/FarmersDelightRefabricated/issues/77
+        return () -> new Object[]{new ItemStack[]{new ItemStack(((Supplier<Item>)() -> Items.COMPASS).get())}};
+    }
+
+    public static Supplier<Object[]> getMealsCategoryStacks() {
+        return () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.VEGETABLE_NOODLES.get())}};
+    }
+
+    public static Supplier<Object[]> getDrinksCategoryStacks() {
+        return () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.APPLE_CIDER.get())}};
+    }
+
+    public static Supplier<Object[]> getMiscCategoryStacks() {
+        return () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.DUMPLINGS.get()), new ItemStack(ModItems.TOMATO_SAUCE.get())}};
+    }
 }
