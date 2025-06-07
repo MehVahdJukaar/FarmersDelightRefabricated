11cb1c6abee8ccdf333360d4f3a04a9adcbeefe0
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2025-06-08 02:42:36 +1000

    Fix #174.

--- src/main/java/vectorwing/farmersdelight/refabricated/FarmersDelightASM.java
+++ src/main/java/vectorwing/farmersdelight/refabricated/FarmersDelightASM.java
@@ -1,6 +1,7 @@
 package vectorwing.farmersdelight.refabricated;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.world.item.Item;
@@ -22,12 +23,14 @@ public class FarmersDelightASM implements Runnable {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
         String recipeBookTypeTarget = remapper.mapClassName("intermediary", "net.minecraft.class_5421");
         ClassTinkerers.enumBuilder(recipeBookTypeTarget).addEnum(COOKING_RECIPE_BOOK_TYPE).build();
-        String recipeBookCategoriesTarget = remapper.mapClassName("intermediary", "net.minecraft.class_314");
-        String itemStackParamType = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_SEARCH_RECIPE_BOOK_CATEGORY, getSearchCategoryStacks()).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MEALS_RECIPE_BOOK_CATEGORY, getMealsCategoryStacks()).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_DRINKS_RECIPE_BOOK_CATEGORY, getDrinksCategoryStacks()).build();
-        ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MISC_RECIPE_BOOK_CATEGORY, getMiscCategoryStacks()).build();
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            String recipeBookCategoriesTarget = remapper.mapClassName("intermediary", "net.minecraft.class_314");
+            String itemStackParamType = "[L" + remapper.mapClassName("intermediary", "net.minecraft.class_1799") + ";";
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_SEARCH_RECIPE_BOOK_CATEGORY, getSearchCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MEALS_RECIPE_BOOK_CATEGORY, getMealsCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_DRINKS_RECIPE_BOOK_CATEGORY, getDrinksCategoryStacks()).build();
+            ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MISC_RECIPE_BOOK_CATEGORY, getMiscCategoryStacks()).build();
+        }
     }
 
     public static Supplier<Object[]> getSearchCategoryStacks() {
