8304e94573a35b421b8effb31f9b76b69432d44d
Anastasia Calico <calico@merchantcalico.net>
2025-06-08 19:57:51 +1000

    Rewrite inventory code to make Fabric Transfer API come second rather than first. (#179)
    * Rewrite inventory code to make Fabric Transfer API come second rather than first.
    
    * Bump version to 3.1.0.
    
    * Minor refactors.
    
    * Fix crashes within CookingPotItemHandler and Hoppers putting containers in the wrong slot.
    
    * Add extra deprecated methods to ItemHandler.
    
    * Fix incorrect early return.
    
    * Fix Comparators not updating from cutting boards.

---
+++ src/main/java/vectorwing/farmersdelight/refabricated/inventory/ItemHandlerStackWrapper.java
@@ -0,0 +1,30 @@
+package vectorwing.farmersdelight.refabricated.inventory;
+
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.base.SingleStackStorage;
+import net.minecraft.world.item.ItemStack;
+
+public class ItemHandlerStackWrapper extends SingleStackStorage {
+    private final ItemStackHandler handler;
+    private final int slot;
+
+    public ItemHandlerStackWrapper(ItemStackHandler handler, int slot) {
+        this.handler = handler;
+        this.slot = slot;
+    }
+
+    @Override
+    protected ItemStack getStack() {
+        return handler.getStackInSlot(slot);
+    }
+
+    @Override
+    protected void setStack(ItemStack stack) {
+        handler.setStackInSlot(slot, stack);
+    }
+
+    @Override
+    protected int getCapacity(ItemVariant itemVariant) {
+        return handler.getSlotLimit(slot);
+    }
+}
