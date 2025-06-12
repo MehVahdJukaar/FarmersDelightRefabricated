a66d65ac5ca10a61849816d3be37e77871223c48
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-06-12 14:13:53 +0200

    actually the remained bug is fixed here

--- src/main/java/vectorwing/farmersdelight/common/utility/TextUtils.java
+++ src/main/java/vectorwing/farmersdelight/common/utility/TextUtils.java
@@ -21,6 +21,7 @@ import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 import net.minecraft.world.item.consume_effects.ConsumeEffect;
 import vectorwing.farmersdelight.FarmersDelight;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
