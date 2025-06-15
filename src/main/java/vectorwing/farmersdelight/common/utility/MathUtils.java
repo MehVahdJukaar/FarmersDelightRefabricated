d73a90c01ebf6e5e4f7fc541a42b4f53cbc27672
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2025-06-16 07:10:39 +1000

    Fix https://github.com/vectorwing/FarmersDelight/issues/1139

--- src/main/java/vectorwing/farmersdelight/common/utility/MathUtils.java
+++ src/main/java/vectorwing/farmersdelight/common/utility/MathUtils.java
@@ -30,7 +30,7 @@ public class MathUtils
 			for (int j = 0; j < handler.getSlots(); ++j) {
 				ItemStack itemstack = handler.getStackInSlot(j);
 				if (!itemstack.isEmpty()) {
-					f += (float) itemstack.getCount() / (float) Math.min(handler.getSlotLimit(j), itemstack.getMaxStackSize());
+					f += (float) itemstack.getCount() / (float) handler.getStackLimit(j, itemstack);
 					++i;
 				}
 			}
