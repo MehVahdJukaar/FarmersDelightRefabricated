b575642b23e55d7db71dd77d295bd95f64e9eca7
Cassian Godsted <cassian@godsted.com>
2025-06-15 06:42:40 -0400

    Add support for Extended Item View (#184)
    * EIV Cooking Pot support
    
    * EIV Info Recipes
    
    * EIV Cutting Board recipes
    
    * Update InfoViewType.java
    
    * Decomposition recipe
    
    * Fix info recipes not appearing
    
    * Render slot backgrounds
    
    * Render items through EIV and slots through graphics
    
    * Rollable recipes
    
    * Fix cooking pot filling
    
    * Swap result item
    
    * Utilize optional slots
    
    * Update CuttingViewRecipe.java
    
    * Workaround for container being required for autofill.
    
    ---------
    
    Co-authored-by: Anastasia Calico <calico@merchantcalico.net>

---
+++ src/main/java/vectorwing/farmersdelight/integration/eiv/info/InfoViewType.java
@@ -0,0 +1,58 @@
+package vectorwing.farmersdelight.integration.eiv.info;
+
+import de.crafty.eiv.common.api.recipe.IEivRecipeViewType;
+import de.crafty.eiv.common.recipe.inventory.RecipeViewMenu;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import vectorwing.farmersdelight.FarmersDelight;
+
+public class InfoViewType implements IEivRecipeViewType {
+
+    protected static final InfoViewType INSTANCE = new InfoViewType();
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("eiv.category.farmersdelight.info");
+    }
+
+    @Override
+    public int getDisplayWidth() {
+        return 120;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 120;
+    }
+
+    @Override
+    public ResourceLocation getGuiTexture() {
+        return FarmersDelight.res("textures/gui/eiv/info.png");
+    }
+
+    @Override
+    public int getSlotCount() {
+        return 1;
+    }
+
+    @Override
+    public void placeSlots(RecipeViewMenu.SlotDefinition slotDefinition) {
+        //Tell EIV where your slots are located by calling slotDefinition.addItemSlot();
+        //NOTE: Slot position is relative to your gui texture
+
+        slotDefinition.addItemSlot(0, 53, 3);
+
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return FarmersDelight.res("info");
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return Items.BOOK.getDefaultInstance();
+    }
+}
