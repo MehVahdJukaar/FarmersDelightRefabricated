916d20313a2973d4e5bdb450910db4094d801aa4
Anastasia Calico <calico@merchantcalico.net>
2025-06-28 20:30:06 +1000

    Fix #194.

--- src/main/java/vectorwing/farmersdelight/data/ItemTags.java
+++ src/main/java/vectorwing/farmersdelight/data/ItemTags.java
@@ -71,8 +71,7 @@ public class ItemTags extends FabricTagProvider.ItemTagProvider
 		valueLookupBuilder(net.minecraft.tags.ItemTags.PIG_FOOD)
 				.add(ModItems.CABBAGE.get())
 				.add(ModItems.TOMATO.get());
-		valueLookupBuilder(net.minecraft.tags.ItemTags.PIG_FOOD)
-				.add(ModItems.BAKED_COD_STEW.get())
+		valueLookupBuilder(net.minecraft.tags.ItemTags.WOLF_FOOD)
 				.add(ModItems.BEEF_STEW.get());
 		valueLookupBuilder(net.minecraft.tags.ItemTags.RABBIT_FOOD)
 				.add(ModItems.CABBAGE.get());
