714193b843fab093f26725337f598f2edd9857d4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-06-18 14:02:10 -0500

    fix datagen

--- src/main/java/vectorwing/farmersdelight/data/EntityTags.java
+++ src/main/java/vectorwing/farmersdelight/data/EntityTags.java
@@ -3,7 +3,7 @@ package vectorwing.farmersdelight.data;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityTypeIds;
 import vectorwing.farmersdelight.common.tag.ModTags;
 import vectorwing.farmersdelight.refabricated.FDRefabricatedTags;
 
@@ -17,27 +17,27 @@ public class EntityTags extends FabricTagsProvider.EntityTypeTagsProvider
 
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
-		valueLookupBuilder(ModTags.EntityTypes.DOG_FOOD_USERS).add(EntityType.WOLF);
-		valueLookupBuilder(ModTags.EntityTypes.HORSE_FEED_USERS).add(
-				EntityType.HORSE,
-				EntityType.SKELETON_HORSE,
-				EntityType.ZOMBIE_HORSE,
-				EntityType.DONKEY,
-				EntityType.MULE,
-				EntityType.LLAMA);
-		valueLookupBuilder(ModTags.EntityTypes.HORSE_FEED_TEMPTED).add(
-				EntityType.HORSE,
-				EntityType.DONKEY,
-				EntityType.MULE);
-		valueLookupBuilder(FDRefabricatedTags.EntityTypes.DROPS_LEATHER)
+		builder(ModTags.EntityTypes.DOG_FOOD_USERS).add(EntityTypeIds.WOLF);
+		builder(ModTags.EntityTypes.HORSE_FEED_USERS).add(
+				EntityTypeIds.HORSE,
+				EntityTypeIds.SKELETON_HORSE,
+				EntityTypeIds.ZOMBIE_HORSE,
+				EntityTypeIds.DONKEY,
+				EntityTypeIds.MULE,
+				EntityTypeIds.LLAMA);
+		builder(ModTags.EntityTypes.HORSE_FEED_TEMPTED).add(
+				EntityTypeIds.HORSE,
+				EntityTypeIds.DONKEY,
+				EntityTypeIds.MULE);
+		builder(FDRefabricatedTags.EntityTypes.DROPS_LEATHER)
 			.add(
-				EntityType.COW,
-				EntityType.MOOSHROOM,
-				EntityType.HORSE,
-				EntityType.DONKEY,
-				EntityType.MULE,
-				EntityType.LLAMA,
-				EntityType.TRADER_LLAMA
+				EntityTypeIds.COW,
+				EntityTypeIds.MOOSHROOM,
+				EntityTypeIds.HORSE,
+				EntityTypeIds.DONKEY,
+				EntityTypeIds.MULE,
+				EntityTypeIds.LLAMA,
+				EntityTypeIds.TRADER_LLAMA
 			);
 	}
 }
