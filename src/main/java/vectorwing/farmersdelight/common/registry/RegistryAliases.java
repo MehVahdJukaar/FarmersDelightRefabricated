38f24af5d2ce22d0bd437befaf9830303c305437
Cassian <cassian@godsted.com>
2026-06-25 15:17:31 -0400

    Upgrade canvas signs correctly

--- src/main/java/vectorwing/farmersdelight/common/registry/RegistryAliases.java
+++ src/main/java/vectorwing/farmersdelight/common/registry/RegistryAliases.java
@@ -1,6 +1,7 @@
 package vectorwing.farmersdelight.common.registry;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 import vectorwing.farmersdelight.common.utility.RecipeUtils;
 
 public class RegistryAliases
@@ -8,6 +9,12 @@ public class RegistryAliases
 	public static void addRegistryAliases() {
 		addBlockAlias("basket", "bamboo_basket");
 		addItemAlias("basket", "bamboo_basket");
+		addBlockEntityAlias("canvas_sign", "sign");
+		addBlockEntityAlias("hanging_canvas_sign", "hanging_sign");
+	}
+
+	private static void addBlockEntityAlias(String oldName, String newName) {
+		BuiltInRegistries.BLOCK_ENTITY_TYPE.addAlias(RecipeUtils.FDLocation(oldName), Identifier.withDefaultNamespace(newName));
 	}
 
 	public static void addBlockAlias(String oldName, String newName) {
