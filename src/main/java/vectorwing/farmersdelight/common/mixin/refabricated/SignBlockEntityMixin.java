250520866ea45fe214333b8cca17f74e89fa6969
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-09-19 10:28:53 +0200

    fixed white sign text

--- src/main/java/vectorwing/farmersdelight/common/mixin/refabricated/SignBlockEntityMixin.java
+++ src/main/java/vectorwing/farmersdelight/common/mixin/refabricated/SignBlockEntityMixin.java
@@ -1,5 +1,6 @@
 package vectorwing.farmersdelight.common.mixin.refabricated;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -8,31 +9,23 @@ import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.entity.SignText;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vectorwing.farmersdelight.common.block.state.CanvasSign;
 
 @Mixin(SignBlockEntity.class)
 public abstract class SignBlockEntityMixin extends BlockEntity {
-	@Shadow
-	public SignText frontText;
-
-	@Shadow
-	public SignText backText;
-
 	public SignBlockEntityMixin(BlockEntityType<?> type, BlockPos worldPosition, BlockState blockState) {
 		super(type, worldPosition, blockState);
 	}
 
-	@Inject(method = "<init>(Lnet/minecraft/world/level/block/entity/BlockEntityType;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)V", at = @At("RETURN"))
-	private void fdrf$fixSignTextColor(BlockEntityType<?> type, BlockPos worldPosition, BlockState blockState, CallbackInfo ci) {
+	@ModifyExpressionValue(method = {"<init>(Lnet/minecraft/world/level/block/entity/BlockEntityType;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)V", "loadAdditional", "applyImplicitComponents"},
+			at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/block/entity/SignText;EMPTY:Lnet/minecraft/world/level/block/entity/SignText;"))
+	private SignText fdrf$fixSignTextColor(SignText signText) {
 		if (getBlockState().getBlock() instanceof CanvasSign canvasSignBlock) {
 			if (canvasSignBlock.isDarkBackground()) {
-				frontText = frontText.asMutable().setColor(DyeColor.WHITE).asImmutable();
-				backText = backText.asMutable().setColor(DyeColor.WHITE).asImmutable();
+				return signText.asMutable().setColor(DyeColor.WHITE).asImmutable();
 			}
 		}
+		return signText;
 	}
 }
